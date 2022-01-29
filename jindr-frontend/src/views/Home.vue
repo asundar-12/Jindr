3432c8429179b12253f6b999f0df14d83146d911
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2022-01-29 01:19:05 -0500

    initiated frontend

---
+++ jindr-frontend/src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div> -->
+  <main>
+	  <h1>JINDR</h1>
+  </main>
+  
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+	@import "../assets/styles.scss";
+	main{	
+		background-color: aquamarine;
+	}
+</style>
